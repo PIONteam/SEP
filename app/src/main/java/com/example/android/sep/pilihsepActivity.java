fa1bedf3ab7f08c97e3635cff5474e4f08265b01
Anggita Kusumaningrum <anggitakyu@gmail.com>
2018-05-04 18:52:37 +0700

    Intent sebagian

--- app/src/main/java/com/example/android/sep/pilihsepActivity.java
+++ app/src/main/java/com/example/android/sep/pilihsepActivity.java
@@ -1,15 +1,65 @@
 package com.example.android.sep;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class pilihsepActivity extends AppCompatActivity {
+    private Button button10;
+    private Button button11;
+    private Button button12;
+    private Button button13;
+    private Button button14;
+    private Button button15;
+    private Button button16;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_pilihsep);
+
+        button10=(Button)findViewById(R.id.button10);
+        button11=(Button)findViewById(R.id.button11);
+        button12=(Button)findViewById(R.id.button12);
+        button13=(Button)findViewById(R.id.button13);
+        button14=(Button)findViewById(R.id.button14);
+        button15=(Button)findViewById(R.id.button15);
+        button16=(Button)findViewById(R.id.button16);
+
+        // function tombol
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sep = new Intent(getApplicationContext(),sep1Activity.class);
+                startActivity(sep);
+            }
+        });
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sep = new Intent(getApplicationContext(),sep1Activity.class);
+                startActivity(sep);
+            }
+        });
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sep = new Intent(getApplicationContext(),sep1Activity.class);
+                startActivity(sep);
+            }
+        });
+        button13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sep = new Intent(getApplicationContext(),sep1Activity.class);
+                startActivity(sep);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/sep/splashActivity.java
+++ app/src/main/java/com/example/android/sep/splashActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class splashActivity extends AppCompatActivity {
 
@@ -12,15 +13,19 @@ public class splashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), loginActivity.class));
-                finish();
-            }
-        }, 3000L); //3000 L = 3 detik
-    }
+                    startActivity(new Intent(getApplicationContext(), pilihsepActivity.class));
+                    finish();
+                }
+
+            }, 3000);
+
+        }
 }
