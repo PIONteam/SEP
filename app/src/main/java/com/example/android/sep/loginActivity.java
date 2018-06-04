3ba393b230a5a392467967a4bc71a11328bbc7d0
Aditia Afif Arfiansyah <aditiaafif98@gmail.com>
2018-06-04 22:31:24 +0700

    profil dan verivikasi

--- app/src/main/java/com/example/android/sep/loginActivity.java
+++ app/src/main/java/com/example/android/sep/loginActivity.java
@@ -46,12 +46,17 @@ public class loginActivity extends AppCompatActivity {
 
     public final static String TAG_NAMA_PENGGUNA = "nama_pengguna";
     public final static String TAG_KATA_SANDI = "kata_sandi";
+    public final static String TAG_ID_PENGGUNA = "id_pengguna";
+    public final static String TAG_ALAMAT = "alamat";
+    public final static String TAG_NO_TLP = "no_tlp";
+    public final static String TAG_EMAIL = "email";
+    public final static String TAG_NAMA_LENGKAP = "nama_lengkap";
 
     String tag_json_obj = "json_obj_req";
 
     SharedPreferences sharedpreferences;
     Boolean session = false;
-    String kata_sandi, nama_pengguna;
+    String kata_sandi, nama_pengguna, nama_lengkap, id_pengguna, email, alamat, no_tlp;
     public static final String my_shared_preferences = "my_shared_preferences";
     public static final String session_status = "session_status";
 
@@ -78,6 +83,11 @@ public class loginActivity extends AppCompatActivity {
         session = sharedpreferences.getBoolean(session_status, false);
         kata_sandi = sharedpreferences.getString(TAG_KATA_SANDI, null);
         nama_pengguna = sharedpreferences.getString(TAG_NAMA_PENGGUNA, null);
+        id_pengguna = sharedpreferences.getString(TAG_ID_PENGGUNA, null);
+        alamat = sharedpreferences.getString(TAG_ALAMAT, null);
+        no_tlp = sharedpreferences.getString(TAG_NO_TLP, null);
+        nama_lengkap = sharedpreferences.getString(TAG_NAMA_LENGKAP, null);
+
 
         if (session) {
             Intent intent = new Intent(loginActivity.this, homeActivity.class);
@@ -113,7 +123,7 @@ public class loginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                Intent sep = new Intent(loginActivity.this, daftarActivity.class);
+                Intent sep = new Intent(loginActivity.this, daftar2Activity.class);
                 startActivity(sep);
                 finish();
             }
@@ -139,8 +149,14 @@ public class loginActivity extends AppCompatActivity {
 
                     // Check for error node in json
                     if (success == 1) {
-                        String nama_pengguna= jObj.getString(TAG_NAMA_PENGGUNA);
+                        String nama_pengguna = jObj.getString(TAG_NAMA_PENGGUNA);
                         String kata_sandi = jObj.getString(TAG_KATA_SANDI);
+                        String id_pennguna = jObj.getString(TAG_ID_PENGGUNA);
+                        String alamat = jObj.getString(TAG_ALAMAT);
+                        String no_tlp = jObj.getString(TAG_NO_TLP);
+                        String email = jObj.getString(TAG_EMAIL);
+                        String nama_lengkap = jObj.getString(TAG_NAMA_LENGKAP);
+
 
                         Log.e("Successfully Login!", jObj.toString());
 
@@ -151,6 +167,14 @@ public class loginActivity extends AppCompatActivity {
                         editor.putBoolean(session_status, true);
                         editor.putString(TAG_KATA_SANDI, kata_sandi);
                         editor.putString(TAG_NAMA_PENGGUNA, nama_pengguna);
+                        editor.putString(TAG_ID_PENGGUNA, id_pennguna);
+                        editor.putString(TAG_ALAMAT, alamat);
+                        editor.putString(TAG_EMAIL, email);
+                        editor.putString(TAG_NO_TLP, no_tlp);
+                        editor.putString(TAG_NAMA_LENGKAP, nama_lengkap);
+
+
+
                         editor.commit();
 
                         // Memanggil main activity
--- app/src/main/java/com/example/android/sep/profilActivity.java
+++ app/src/main/java/com/example/android/sep/profilActivity.java
@@ -1,16 +1,326 @@
 package com.example.android.sep;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.android.sep.app.App_Controller;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.android.sep.loginActivity.TAG_ALAMAT;
+import static com.example.android.sep.loginActivity.TAG_EMAIL;
+import static com.example.android.sep.loginActivity.TAG_ID_PENGGUNA;
+import static com.example.android.sep.loginActivity.TAG_KATA_SANDI;
+import static com.example.android.sep.loginActivity.TAG_NAMA_LENGKAP;
+import static com.example.android.sep.loginActivity.TAG_NAMA_PENGGUNA;
+import static com.example.android.sep.loginActivity.TAG_NO_TLP;
+import static com.example.android.sep.loginActivity.my_shared_preferences;
+import static com.example.android.sep.loginActivity.session_status;
+
 
 public class profilActivity extends AppCompatActivity {
+
+    ProgressDialog pDialog;
+    int success;
+    private static final String TAG = daftarActivity.class.getSimpleName();
+    private String url1 = Server.URL + "profil.php";
+    private String url2 = Server.URL + "edit_profil.php";
+
+
+    private final static String TAG_SUCCESS = "success";
+
+    SharedPreferences sharedPreferences;
+    String tag_json_obj = "json_obj_req";
+    ConnectivityManager connec;
+    Boolean session = false;
+    String nama_pengguna, nama_lengkap, no_tlp, alamat, kata_sandi, email, id_pengguna;
+    EditText namaLengkap2, namaPengguna2, alamat2, email2, kataSandi2;
+    Button edit, simpan, batal;
+    TextView txtNamaLengkap, txtNamaPengguna, txtAlmat, txtEmail, txtKataSandi;
+    LinearLayout satu, dua;
+
+    @SuppressLint({"CutPasteId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_profil);
 
+        satu = findViewById(R.id.linear1);
+        dua = findViewById(R.id.linear2);
+
+        namaLengkap2 = (EditText) findViewById(R.id.namaLengkap1);
+        namaPengguna2 = (EditText) findViewById(R.id.namaPengguna1);
+        alamat2 = (EditText) findViewById(R.id.alamat1);
+        email2 = (EditText) findViewById(R.id.email1);
+        kataSandi2 = (EditText) findViewById(R.id.kataSandi1);
+
+        edit =(Button) findViewById(R.id.buttonEdit);
+        simpan = (Button) findViewById(R.id.buttonSimpan);
+        batal = (Button) findViewById(R.id.buttonBatal);
+
+
+        txtNamaLengkap = (TextView)findViewById(R.id.textView38);
+        txtNamaPengguna = (TextView)findViewById(R.id.textView45);
+        txtAlmat = (TextView)findViewById(R.id.textView50);
+        txtEmail = (TextView)findViewById(R.id.textView48);
+        txtKataSandi = (TextView)findViewById(R.id.textView51);
+
+        sharedPreferences = getSharedPreferences(my_shared_preferences, Context.MODE_PRIVATE);
+        session = sharedPreferences.getBoolean(session_status, false);
+        nama_pengguna = sharedPreferences.getString(TAG_NAMA_PENGGUNA, null);
+        nama_lengkap = sharedPreferences.getString(TAG_NAMA_LENGKAP, null);
+        alamat = sharedPreferences.getString(TAG_ALAMAT, null);
+        kata_sandi = sharedPreferences.getString(TAG_KATA_SANDI, null);
+        email = sharedPreferences.getString(TAG_EMAIL, null);
+        id_pengguna = sharedPreferences.getString(TAG_ID_PENGGUNA, null);
+
+
+        txtNamaLengkap.setText(nama_lengkap);
+        txtNamaPengguna.setText(nama_pengguna);
+        txtAlmat.setText(alamat);
+        txtEmail.setText(email);
+        txtKataSandi.setText(kata_sandi);
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUI("EDIT");
+            }
+        });
+
+        batal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUI("BATAL");
+            }
+        });
+
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama_lengkap = namaLengkap2.getText().toString();
+                String nama_pengguna = namaPengguna2.getText().toString();
+                String alamat = alamat2.getText().toString();
+                String email = email2.getText().toString();
+                String kata_sandi = kataSandi2.getText().toString();
+                connec = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                {
+                    if (connec.getActiveNetworkInfo()   !=null
+                            && connec.getActiveNetworkInfo() .isAvailable()
+                            && connec.getActiveNetworkInfo(). isAvailable()){
+                        simpan (id_pengguna, nama_lengkap, nama_pengguna, alamat, email, no_tlp, kata_sandi);
+
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Tidak Ada Konneksi Internet", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        });
+
+        connec = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        {
+            if (connec.getActiveNetworkInfo() !=null
+                    && connec.getActiveNetworkInfo().isAvailable()
+                    && connec.getActiveNetworkInfo().isAvailable()){
+                tampilkanData(id_pengguna);
+            }else{
+                Toast.makeText(getApplicationContext(), "Tidak Ada Koneksi Internet", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void tampilkanData (final String id_pengguna){
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        pDialog.setMessage("Mengambil Data");
+        showDialog();
+
+        StringRequest request= new StringRequest(Request.Method.POST, url1, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e(TAG, "Respon Data:" + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+
+                    String nama_lengkap = jObj.getString(TAG_NAMA_LENGKAP);
+                    String nama_pengguna = jObj.getString(TAG_NAMA_PENGGUNA);
+                    String alamat = jObj.getString(TAG_ALAMAT);
+                    String email = jObj.getString(TAG_EMAIL);
+                    String no_tlp = jObj.getString(TAG_NO_TLP);
+                    String kata_sandi = jObj.getString(TAG_KATA_SANDI);
+
+                    txtNamaLengkap.setText(nama_lengkap);
+                    txtNamaPengguna.setText(nama_pengguna);
+                    txtAlmat.setText(alamat);
+                    txtEmail.setText(email);
+                    txtKataSandi.setText(kata_sandi);
+
+                    Log.e("Sukses Ambil Data", jObj.toString());
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+                    protected Map<String, String> getParams(){
+
+                Map<String, String> params = new HashMap<>();
+                params.put("Id_Pengguna", id_pengguna);
+
+                return params;
+            }
+        };
+
+        App_Controller.getInstance().addToRequestQueue(request, tag_json_obj);
+    }
+
+    private void simpan (final String id_pennguna, final String nama_lengkap, final String nama_pengguna,
+                         final String alamat, final String email, final String no_tlp, final String kata_sandi){
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        pDialog.setMessage("Simpan");
+        showDialog();
+
+
+
+        StringRequest strReq = new StringRequest(Request.Method.POST, url2, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.e(TAG, "Respon Data: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    success = jObj.getInt(TAG_SUCCESS);
+                    if(success == 1){
+
+                        String nama_lengkap = jObj.getString(TAG_NAMA_LENGKAP);
+                        String nama_pengguna = jObj.getString(TAG_NAMA_PENGGUNA);
+                        String alamat = jObj.getString(TAG_ALAMAT);
+                        String email = jObj.getString(TAG_EMAIL);
+                        String kata_sandi = jObj.getString(TAG_KATA_SANDI);
+
+                        txtNamaLengkap.setText(nama_lengkap);
+                        txtNamaPengguna.setText(nama_pengguna);
+                        txtAlmat.setText(alamat);
+                        txtEmail.setText(email);
+                        txtKataSandi.setText(kata_sandi);
+
+                        sharedPreferences = getSharedPreferences(my_shared_preferences, Context.MODE_PRIVATE);
+                        session = sharedPreferences.getBoolean(session_status, false);
+                        nama_pengguna = sharedPreferences.getString(TAG_NAMA_PENGGUNA, null);
+                        nama_lengkap = sharedPreferences.getString(TAG_NAMA_LENGKAP, null);
+                        alamat = sharedPreferences.getString(TAG_ALAMAT, null);
+                        kata_sandi = sharedPreferences.getString(TAG_KATA_SANDI, null);
+                        email = sharedPreferences.getString(TAG_EMAIL, null);
+                        id_pengguna = sharedPreferences.getString(TAG_ID_PENGGUNA, null);
+
+                        updateUI("BATAL");
+
+                        Log.e("Sukses Ambil Data", jObj.toString());
+
+
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("id_pengguna", id_pengguna);
+                params.put("nama_lengkap", nama_lengkap);
+                params.put("alamat", alamat);
+                params.put("no_tlp", no_tlp);
+                params.put("email", email);
+                params.put("kata_sandi", kata_sandi);
+                params.put("nama_pengguna", nama_pengguna);
+
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        App_Controller.getInstance().addToRequestQueue(strReq, tag_json_obj);
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+    private void updateUI(String uiState) {
+        switch (uiState) {
+            case "EDIT":
+                // Initialized state, show only the phone number field and start button
+                satu.setVisibility(View.GONE);
+                dua.setVisibility(View.VISIBLE);
+                namaLengkap2.setText(txtNamaLengkap.getText().toString());
+                namaPengguna2.setText(txtNamaPengguna.getText().toString());
+                alamat2.setText(txtAlmat.getText().toString());
+                email2.setText(txtEmail.getText().toString());
+                kataSandi2.setText(txtKataSandi.getText().toString());
+
+                break;
+            case "BATAL":
+                // Code sent state, show the verification field, the
+                satu.setVisibility(View.VISIBLE);
+                dua.setVisibility(View.GONE);
+
+                break;
+        }
     }
 }
